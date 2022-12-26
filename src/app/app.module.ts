eb5d7a0978fbad80b09d41f1f931742d2127cd4e
MaxiProgram <maxiprogram@gmail.com>
2022-12-26 18:32:12 +0300

    edit main menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTooltipModule}  from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -27,6 +29,8 @@ import { PagesModule } from './pages/pages.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
     MatTooltipModule,
     MatMenuModule,
     MatDividerModule,
