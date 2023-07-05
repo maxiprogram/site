b7b107f568792181d7d5a3bc7767cc569dba66d8
MaxiProgram <maxiprogram@gmail.com>
2023-07-05 19:42:52 +0300

    post heineken

--- src/app/pages/project-page/post-page/heineken/heineken.component.ts
+++ src/app/pages/project-page/post-page/heineken/heineken.component.ts
@@ -22,7 +22,28 @@ export class HeinekenComponent {
     video: 'https://youtu.be/jqlibz3ICOQ',
     //posterImage: 'assets/img/slider/2_min.jpeg',
     //title: 'Image title'
-  }];
+  },
+  {
+    video: 'https://www.youtube.com/watch?v=Ax6Bei8gms4',
+    //posterImage: 'assets/img/slider/2_min.jpeg',
+    //title: 'Image title'
+  },
+  {
+    video: 'https://www.youtube.com/watch?v=MruPSpJDrJQ',
+    //posterImage: 'assets/img/slider/2_min.jpeg',
+    //title: 'Image title'
+  },
+  {
+    video: 'https://www.youtube.com/watch?v=22N4xxzJ-r4',
+    //posterImage: 'assets/img/slider/2_min.jpeg',
+    //title: 'Image title'
+  },
+  {
+    video: 'https://www.youtube.com/watch?v=GsWirZvXiTk',
+    //posterImage: 'assets/img/slider/2_min.jpeg',
+    //title: 'Image title'
+  }
+];
 
 
 }
